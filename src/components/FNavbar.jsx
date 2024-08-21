6ff401e8cb1ffdd880cc526e55e9c2b911978e2a
Samar <osmansamar367@gmail.com>
2024-08-21 11:18:50 +0200

    Updated the NavBarToggle

--- src/components/FNavbar.jsx
+++ src/components/FNavbar.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Nav, Navbar, Button } from "react-bootstrap";
 import logo from "../assets/Fathers_House_Logo_V1.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const FNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Navbar
       expand="lg"
       style={{
         backgroundColor: "#F8F4EE",
-        fontSize: "16px",
-        // padding: "10px 20px",
+        fontSize: "1rem",
       }}
     >
-      <Container>
-        {/* fluid */}
+      <Container style={{ padding: " 0.30rem 2rem 0.30rem 2rem" }}>
         <Navbar.Brand href="/">
-          {/* style={{ paddingLeft: "100px" }} */}
           <img
             src={logo}
             alt="Fathers_House_Logo"
@@ -26,31 +30,55 @@ const FNavbar = () => {
             }}
           />
         </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {/* Toggle button with custom icon  */}
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle> */}
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={handleToggle}
+          style={{
+            width: "40px",
+            height: "40px",
+            borderRadius: "50%",
+            padding: "0",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {isOpen ? <FaTimes size={20} /> : <FaBars size={20} />}
+        </Navbar.Toggle>
 
         <Navbar.Collapse
           id="basic-navbar-nav"
           className="justify-content-center"
         >
           <Nav
-            className="text-center"
+            className="text-center "
             style={{
               color: "#463F2F",
-              fontSize: "1rem",
+              fontSize: "0.875rem",
               fontFamily: "Inter, sans-serif",
               fontWeight: 500,
             }}
           >
-            <Nav.Link href="#watch">Watch</Nav.Link>
-            <Nav.Link href="#mission">Mission</Nav.Link>
-            <Nav.Link href="#ministry">Ministry</Nav.Link>
-            <Nav.Link href="#team">Team</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link
+              href="#watch"
+              style={{
+                margin: "0 1rem",
+              }}
+              // className="mx-sm-4"
+            >
+              Watch
+            </Nav.Link>
+            <Nav.Link href="#mission" style={{ margin: "0 1rem" }}>
+              Mission
+            </Nav.Link>
+            <Nav.Link href="#ministry" style={{ margin: "0 1rem" }}>
+              Ministry
+            </Nav.Link>
+            <Nav.Link href="#team" style={{ margin: "0 1rem" }}>
+              Team
+            </Nav.Link>
+            <Nav.Link href="#contact" style={{ margin: "0 1rem" }}>
+              Contact
+            </Nav.Link>
           </Nav>
 
           {/* Button for small and medium screens (including tablets) */}
@@ -71,7 +99,6 @@ const FNavbar = () => {
             </Button>
           </div>
         </Navbar.Collapse>
-
         {/* Button for larger screens (lg and up) */}
         <div className="me-auto d-none d-lg-block">
           <Button
