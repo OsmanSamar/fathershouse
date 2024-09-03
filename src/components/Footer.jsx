22df0dcaad445e0b9e9125e51430fce1c451ee42
Samar <osmansamar367@gmail.com>
2024-09-03 12:10:40 +0200

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -176,7 +176,7 @@ const Footer = () => {
                     lineHeight: "1.5",
                     fontFamily: "Inter, sans-serif",
                     fontWeight: 100,
-                    fontSize: "0.875rem",
+                    fontSize: "1rem",
                     letterSpacing: "0px",
                     opacity: 1,
                     margin: "2rem 0 2rem 2rem",
